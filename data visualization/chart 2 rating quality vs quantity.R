c096aceac959bdeb5b880f80dab344b820257d70
Zijun Wei <101439362+HULIBABAs@users.noreply.github.com>
2022-05-20 15:38:16 -0700

    Update chart 2 rating quality vs quantity.R

--- data visualization/chart 2 rating quality vs quantity.R
+++ data visualization/chart 2 rating quality vs quantity.R
@@ -7,11 +7,11 @@ mydata <- read_csv(url(urlfile))
 
 View(mydata)
 
-plot(mydata$`total ratings`, mydata$`average rating`,
-     xlim = c(0, 90000000) , ylim = c(0, 5), 
+plot(mydata$`total ratings`, mydata$`5 star ratings`,
+     xlim = c(0, 90000000) , ylim = c(0, 70000000), 
      pch = 18, 
      cex = 0.8, 
      col = "#69b3a2",
-     xlab = "Total ratings", ylab = "Average ratings",
+     xlab = "Total ratings", ylab = "5 star ratings",
      main = "Relation between number of ratings and quality of ratings"
 )
